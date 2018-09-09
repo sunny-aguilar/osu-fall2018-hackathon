c4fb37ca740fe060b72ddae2c1920ad7766de201
Sunny <sunny.aguilar@gmail.com>
2018-09-08 21:12:07 -0700

    add cin ignore to menu

--- BankApp.cpp
+++ BankApp.cpp
@@ -179,6 +179,7 @@ void BankApp::subMenu() {
         cout << RESET << endl;
 
         cin >> selection;
+        cin.ignore();
 
         // use toupper function to make all letters capital
         selection = toupper(selection);
